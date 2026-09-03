0e1af1e37428bb61f2c18f8ab12ba7a2e89c2f1d
alexchandan <alexchandan321@gmail.com>
2026-09-03 14:25:21 +0530

    refactor(recruiter-ui): improved recruiter UI

---
+++ client/src/components/ui/skeletons/index.ts
@@ -0,0 +1,12 @@
+export * from "./Skeleton";
+export { default as CandidateDashboardSkeleton } from "./CandidateDashboardSkeleton";
+export { default as CandidateProfileSkeleton } from "./CandidateProfileSkeleton";
+export {
+  default as JobBoardSkeleton,
+  JobCardSkeleton,
+  JobGridSkeleton,
+} from "./JobBoardSkeleton";
+export { default as JobDetailSkeleton } from "./JobDetailSkeleton";
+export { default as RecruiterDashboardSkeleton } from "./RecruiterDashboardSkeleton";
+export { default as RecruiterProfileSkeleton } from "./RecruiterProfileSkeleton";
+export { NotificationSkeleton } from "./NotificationSkeleton";
